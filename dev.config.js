49cf57d4bb6ad6ba8e6ce854a7ad0b97d33c6621
Jorden <firenzo.jorden@capgemini.com>
2021-09-16 15:17:28 +0200

    change

--- dev.config.js
+++ dev.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [
       {
-        name: 'run develop',
+        name: 'develop',
         script: 'npm',
-        args: 'develop',
+        args: 'run develop',
       }, {
         script: 'npm',
         // seperate error logs
