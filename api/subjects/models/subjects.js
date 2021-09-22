0c6db5503645f3c12345a26ea06bccaac523da85
Jorden <firenzo.jorden@capgemini.com>
2021-09-22 15:44:49 +0200

    added comments to script files

--- api/subjects/models/subjects.js
+++ api/subjects/models/subjects.js
@@ -13,6 +13,7 @@ module.exports = {
     async afterUpdate(data) {
       console.log("afterUpdate")
 
+      //Code to test automatic building locally on Windows
       if(process.env.OS && process.env.USERNAME){
         if (process.env.OS.includes("Windows") && process.env.USERNAME === "FJORDEN") {
           exec("start cmd /k build.bat")
@@ -20,8 +21,8 @@ module.exports = {
       }
 
       if(process.env.OS === undefined && process.env.USER === "tubadmin"){
+        //starts build process for Nuxt application on Ubuntu server
         exec("pm2 stop build || : && pm2 start build --no-autorestart")
-        // exec("pm2 start build --no-autorestart")
         console.log("Generated Website")
       }
     },
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#when updated, make file excecutable with chmod +x build.sh
 cd "../team-uitbehandeld" && npm run generate
 exit
 
