9fc81f80d78d7d1091a97df3a74292a2c35467a7
Jorden <firenzo.jorden@capgemini.com>
2021-10-11 16:09:29 +0200

    optimized build commands

--- build.sh
+++
@@ -1,5 +0,0 @@
-#when updated, make file excecutable with chmod +x build.sh
-#git update-index --chmod=+x foo.sh
-pm2 stop build || : && pm2 start build --no-autorestart
-exit
-
--- modules/build.js
+++ modules/build.js
@@ -1,16 +1,19 @@
 const { exec } = require('child_process');
 
+const windowsCommand = "start cmd /k build.bat";
+const ubuntuCommand = "pm2 stop build || : && pm2 start build --no-autorestart"
+
 const buildNuxtApp = () => {
   //Code to test automatic building locally on Windows
   if(process.env.OS && process.env.USERNAME){
     if (process.env.OS.includes("Windows") && process.env.USERNAME === "FJORDEN") {
-      exec("start cmd /k build.bat")
+      exec(windowsCommand)
     }
   }
 
   //Code for building Nuxt Application on the Server
   if(process.env.OS === undefined && process.env.USER === "tubadmin"){
-    exec("./build.sh")
+    exec(ubuntuCommand)
     console.log("Generated Website")
   }
 }
