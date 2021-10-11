45de47fc5b96af2e1ed0de7bc78a902c9e87b3cb
Jorden <firenzo.jorden@capgemini.com>
2021-10-11 15:09:14 +0200

    build command in module

--- api/contact-info/models/contact-info.js
+++ api/contact-info/models/contact-info.js
@@ -1,31 +1,20 @@
 'use strict';
-const { exec } = require('child_process');
+const { buildNuxtApp } = require('./../../../modules/build.js');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
 module.exports = {
-    lifecycles:{
+    lifecycles: {      
         async afterUpdate(data) {
             console.log("afterUpdate")
-
-            //Code to test automatic building locally on Windows
-            if(process.env.OS && process.env.USERNAME){
-                if (process.env.OS.includes("Windows") && process.env.USERNAME === "FJORDEN") {
-                    exec("start cmd /k build.bat")
-                }
-            }
-
-            if(process.env.OS === undefined && process.env.USER === "tubadmin"){
-                //starts build process for Nuxt application on Ubuntu server
-                exec("pm2 stop build || : && pm2 start build --no-autorestart")
-                console.log("Generated Website")
-            }
+            buildNuxtApp();
         },
-
+    
         async afterDelete(data) {
             console.log("afterDelete")
+            buildNuxtApp();
         },
     }
 };
---
+++ modules/build.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+
+const buildNuxtApp = () => {
+  //Code to test automatic building locally on Windows
+  if(process.env.OS && process.env.USERNAME){
+    if (process.env.OS.includes("Windows") && process.env.USERNAME === "FJORDEN") {
+      exec("start cmd /k build.bat")
+    }
+  }
+
+  //Code for building Nuxt Application on the Server
+  if(process.env.OS === undefined && process.env.USER === "tubadmin"){
+    exec("pm2 stop build || : && pm2 start build --no-autorestart")
+    console.log("Generated Website")
+  }
+}
+
+module.exports.buildNuxtApp = buildNuxtApp;
