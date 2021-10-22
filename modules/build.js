d20288244abd3a54bb5dedc351314949305b3e23
Jorden <firenzo.jorden@capgemini.com>
2021-10-22 15:36:49 +0200

    make use of process.platform

--- modules/build.js
+++ modules/build.js
@@ -1,18 +1,18 @@
 const { exec } = require('child_process');
 
+const testAutomaticBuilding = true;
 const windowsCommand = "start cmd /k build.bat";
 const ubuntuCommand = "pm2 stop build || : && pm2 start build --no-autorestart"
 
 const buildNuxtApp = () => {
+
   //Code to test automatic building locally on Windows
-  if(process.env.OS && process.env.USERNAME){
-    if (process.env.OS.includes("Windows") && process.env.USERNAME === "FJORDEN") {
-      exec(windowsCommand)
-    }
+  if (process.platform === 'win32' && testAutomaticBuilding) {
+    exec(windowsCommand)
   }
 
   //Code for building Nuxt Application on the Server
-  if(process.env.OS === undefined && process.env.USER === "tubadmin"){
+  if (process.platform === 'linux') {
     exec(ubuntuCommand)
     console.log("Generated Website")
   }
